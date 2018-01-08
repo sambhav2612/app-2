54086ba3bc7c9cfc356eb313b950cef9c90fec1c
Sambhav Jain <sambhavjain2612@gmail.com>
2018-01-08 22:50:35 +0530

    finalise api demo

---
+++ index.js
@@ -0,0 +1,46 @@
+const app = document.getElementById('root');
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(container);
+
+var request = new XMLHttpRequest();
+
+request.open('GET', 'api.json', true);
+
+request.onload = function () {
+    var data = JSON.parse(this.response);
+
+    if (request.status >= 200 && request.status < 404) {
+        data.forEach(contri => {
+            const card = document.createElement('card');
+            card.setAttribute('class', 'card');
+
+            const h1 = document.createElement('h1');
+            h1.textContent = contri.name;
+
+            const p = document.createElement('p');
+            p.textContent = contri.description;
+
+            const quote = document.createElement('blockquote');
+            quote.textContent = contri.quote;
+
+            const button = document.createElement('button');
+            button.textContent = contri.github_username;
+            document.addEventListener("click", function() {
+                window.open(contri.github_url);
+            });
+
+            container.appendChild(card);
+            card.appendChild(h1);
+            card.appendChild(p);
+            card.appendChild(quote);
+            card.appendChild(button);
+        });
+    } else {
+        console.log("Bad Request");
+    }
+}
+
+request.send();
